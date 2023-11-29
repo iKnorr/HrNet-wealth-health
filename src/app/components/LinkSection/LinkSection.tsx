4627d329f849d954a66232e5ad0ae1476f047bd3
Ingo Knorr <ingoknorr@gmail.com>
2023-11-29 15:29:35 +0100

    added new ik-semect-wealth-health package

--- src/app/components/LinkSection/LinkSection.tsx
+++ src/app/components/LinkSection/LinkSection.tsx
@@ -5,7 +5,7 @@ import styles from './LinkSection.module.css';
 export const LinkSection = () => {
   return (
     <div className={styles.linkWrapper}>
-      <Link href="/employee/list">
+      <Link href="/">
         <h3>Home</h3>
       </Link>
       <Link href="/employee/list">
